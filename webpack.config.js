f3bb9300c5ae4df63daeddc1fc778624dc98ec72
genekomarov <gene.komarov@gmail.com>
2021-03-18 21:36:00 +0500

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const path = require('path')
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
